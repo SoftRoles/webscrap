db1a0337215cdc10ffc2e0cd5c31baea8f6d6b79
Hüseyin Yiğit <yigit.hsyn@gmail.com>
2018-04-02 00:41:48 +0200

    Webshot library changed to pageres

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-var webshot = require('webshot');
+const Pageres = require('pageres');
 
-webshot('haberturk.com', 'tmp/haber7.png', function(err) {
-  if(err) console.log(err)
-});
+const pageres = new Pageres({delay: 5, crop:true})
+	// .src('yeoman.io', ['480x320', '1024x768', 'iphone 5s'], {crop: true})
+	// .src('todomvc.com', ['1280x1024', '1920x1080'])
+	.src('haber7.com',["640x360"])
+	// .src('data:text/html;base64,PGgxPkZPTzwvaDE+', ['1024x768'])
+	.dest(__dirname+"/tmp")
+	.run()
+	.then(() => console.log('done'));
