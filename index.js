9bb34d378aa0f2872f89ee0eb69ae8c8e49bb4b0
Hüseyin <yigit.hsyn@gmail.com>
2019-01-29 23:49:58 +0300

    session store

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 //=============================================================================
 const express = require('express');
 const argparse = require('argparse').ArgumentParser
+const assert = require('assert')
+const session = require('express-session');
+const mongodbSessionStore = require('connect-mongodb-session')(session);
+const passport = require('passport');
 const cheerio = require("cheerio")
 const request = require("request")
 
@@ -16,15 +20,80 @@ const argParser = new argparse({
 argParser.addArgument(['-p', '--port'], { help: 'Listening port', defaultValue: '3003' })
 const args = argParser.parseArgs()
 
+//-------------------------------------
+// mongodb
+//-------------------------------------
+let mongodb;
+const mongoClient = require("mongodb").MongoClient
+const mongoObjectId = require("mongodb").ObjectID
+const mongodbUrl = "mongodb://127.0.0.1:27017"
+mongoClient.connect(mongodbUrl, { poolSize: 10, useNewUrlParser: true }, function (err, client) {
+  assert.equal(null, err);
+  mongodb = client;
+});
+
 //=============================================================================
 // http server
 //=============================================================================
 const app = express();
 
 //-------------------------------------
+// session store
+//-------------------------------------
+var store = new mongodbSessionStore({
+  uri: mongodbUrl,
+  databaseName: 'auth',
+  collection: 'sessions'
+});
+
+// Catch errors
+store.on('error', function (error) {
+  assert.ifError(error);
+  assert.ok(false);
+});
+
+var sessionOptions = {
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+  },
+  store: store,
+  resave: true,
+  saveUninitialized: true
+}
+
+app.use(session(sessionOptions));
+
+//-------------------------------------
+// authentication
+//-------------------------------------
+passport.serializeUser(function (user, cb) {
+  cb(null, user.username);
+});
+
+passport.deserializeUser(function (username, cb) {
+  mongodb.db("auth").collection("users").findOne({ username: username }, function (err, user) {
+    if (err) return cb(err)
+    if (!user) { return cb(null, false); }
+    return cb(null, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(require('@softroles/authorize-bearer-token')(function (token, cb) {
+  mongodb.db("auth").collection("users").findOne({ token: token }, function (err, user) {
+    if (err) return cb(err)
+    if (!user) { return cb(null, false); }
+    return cb(null, user);
+  });
+}))
+
+//-------------------------------------
 // common middlewares
 //-------------------------------------
-app.use(require('@softroles/authorize-local-user')())
+// app.use(require('@softroles/authorize-local-user')())
 app.use(require('morgan')('tiny'));
 app.use(require('body-parser').json())
 app.use(require('body-parser').urlencoded({ extended: true }));
